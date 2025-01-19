59ed553e5c0a1dbe204ea3461a70771e2c48b682
Michael Conrad <m.conrad.202@gmail.com>
2025-01-18 21:05:58 -0500

    2.6 Moving the Player

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -1,15 +1,22 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody3D
 {
+    private Vector3 _direction = Vector3.Zero;
+    
     public override void _PhysicsProcess(double delta)
     {
-        base._PhysicsProcess(delta);
+        Velocity = _direction;
+        MoveAndSlide();
     }
 
     public override void _Input(InputEvent @event)
     {
-        base._Input(@event);
+        var input = Input.GetVector(
+            "MoveLeft",
+            "MoveRight",
+            "MoveForward",
+            "MoveBackward");
+        _direction = new Vector3(input.X, 0, input.Y) * 5;
     }
 }
