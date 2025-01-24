a0527376753e8b3579a450d9c8f15e07beceaa2e
Michael Conrad <m.conrad.202@gmail.com>
2025-01-23 19:26:10 -0500

    5.10 Patrolling the Path

--- Scripts/Characters/Enemy/EnemyPatrolState.cs
+++ Scripts/Characters/Enemy/EnemyPatrolState.cs
@@ -1,12 +1,25 @@
 using GamedevTvActionAdventure25d_RPG.Scripts.General;
+using Godot;
 
 namespace GamedevTvActionAdventure25d_RPG.Scripts.Characters.Enemy;
 
 public partial class EnemyPatrolState : EnemyState {
+    private int pointIndex;
+
     protected override void EnterState() {
         base.EnterState();
+        pointIndex = 1;
         CharacterNode.CharacterSprite.Play(GameConstants.Anim.Move);
-        Destination = GetPointGlobalPosition(1);
+        Destination = GetPointGlobalPosition(pointIndex);
+        CharacterNode.NaviAgent.TargetPosition = Destination;
+        CharacterNode.NaviAgent.NavigationFinished += HandleNavigationFinished;
+    }
+
+    private void HandleNavigationFinished() {
+        GD.Print("Navigation finished");
+        pointIndex = (++pointIndex) % CharacterNode.PathNode.Curve.PointCount;
+        Destination = GetPointGlobalPosition(pointIndex);
+        CharacterNode.NaviAgent.TargetPosition = Destination;
     }
 
     public override void _PhysicsProcess(double delta) {
