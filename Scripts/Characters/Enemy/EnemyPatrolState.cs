e0377bc5812da19e41b6b7c86d41abeab2338340
Michael Conrad <m.conrad.202@gmail.com>
2025-01-29 21:16:23 -0500

    Refactor enemy patrol logic and update player position.
    Refactored enemy patrol state to simplify point index logic using a helper method `_nextPointIndex`. Adjusted the handling of timer signals for improved state transitions. Updated player's starting position and transform in the main level scene for better alignment within the environment. These changes improve code clarity and gameplay consistency.

--- Scripts/Characters/Enemy/EnemyPatrolState.cs
+++ Scripts/Characters/Enemy/EnemyPatrolState.cs
@@ -8,42 +8,47 @@ public partial class EnemyPatrolState : EnemyState
     [Export(PropertyHint.Range, "0,20,0.1")]
     private float _maxIdleTime = 4;
 
-    private int _pointIndex;
+    private int _pointIndex = 0;
     [Export] private Timer _timer;
+
+    private int _nextPointIndex()
+    {
+        return ++_pointIndex % CharacterNode.PathNode.Curve.PointCount;
+    }
 
     public override void _Ready()
     {
         base._Ready();
         CharacterNode.NaviAgent.NavigationFinished += HandleNavigationFinished;
-        _timer.Timeout += HandleTimeout;
     }
 
     protected override void EnterState()
     {
         base.EnterState();
-        _pointIndex = 1;
         CharacterNode.CharacterSprite.Play(GameConstants.Anim.Move);
-        Destination = GetPointGlobalPosition(_pointIndex);
+        Destination = GetPointGlobalPosition(_nextPointIndex());
         CharacterNode.NaviAgent.TargetPosition = Destination;
     }
 
     private void HandleTimeout()
     {
+        GD.Print("Timeout " + CharacterNode.Name);
         CharacterNode.CharacterSprite.Play(GameConstants.Anim.Move);
-        _pointIndex = ++_pointIndex % CharacterNode.PathNode.Curve.PointCount;
-        Destination = GetPointGlobalPosition(_pointIndex);
+        Destination = GetPointGlobalPosition(_nextPointIndex());
         CharacterNode.NaviAgent.TargetPosition = Destination;
+        _timer.Timeout -= HandleTimeout;
     }
 
     private void HandleNavigationFinished()
     {
-        GD.Print("Navigation finished");
+        GD.Print("Navigation finished" + CharacterNode.Name);
         CharacterNode.CharacterSprite.Play(GameConstants.Anim.Idle);
+        CharacterNode.Velocity = Vector3.Zero;
         var rng = new RandomNumberGenerator();
         _timer.WaitTime = rng.RandfRange(0, _maxIdleTime);
+        _timer.Timeout += HandleTimeout;
         _timer.Start();
-        _pointIndex = ++_pointIndex % CharacterNode.PathNode.Curve.PointCount;
-        Destination = GetPointGlobalPosition(_pointIndex);
+        Destination = GetPointGlobalPosition(_nextPointIndex());
     }
 
     public override void _PhysicsProcess(double delta)
@@ -51,6 +56,7 @@ public partial class EnemyPatrolState : EnemyState
         base._PhysicsProcess(delta);
         if (!_timer.IsStopped())
         {
+            return;
         }
         // Move();
     }
