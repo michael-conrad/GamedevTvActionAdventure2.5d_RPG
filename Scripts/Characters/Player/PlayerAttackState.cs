aa97ab84204e5aaf3f6672a2537b2c5a6f509ced
Michael Conrad <m.conrad.202@gmail.com>
2025-02-01 14:10:45 -0500

    6.4 Resetting the Combo Counter with a Timer
    Add combo timer to PlayerAttackState for reset handling
    
    Introduced a Timer node to reset the combo count after a specified duration, enhancing combo management in PlayerAttackState. The timer ensures that the combo resets if no subsequent attack occurs within the defined time frame. Updated state transitions to integrate the timer's behavior, improving the attack flow and usability. This change maintains smooth gameplay and addresses potential combo state inconsistencies.

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -1,21 +1,38 @@
-using GamedevTvActionAdventure25d_RPG.Scripts.Characters.Player;
 using GamedevTvActionAdventure25d_RPG.Scripts.General;
+using Godot;
+
+namespace GamedevTvActionAdventure25d_RPG.Scripts.Characters.Player;
 
 public partial class PlayerAttackState : PlayerState
 {
     private int _comboCount = 0;
+    [Export] private float _comboTime = 0.5f;
     private int _maxComboCount = 2;
+    private Timer _timer;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _timer = new Timer();
+        _timer.OneShot = true;
+        _timer.WaitTime = _comboTime;
+        AddChild(_timer);
+        _timer.Timeout += () => _comboCount = 0;
+    }
 
     protected override void EnterState()
     {
         base.EnterState();
+
         CharacterNode.CharacterSprite.Play(GameConstants.Anim.Attack + (_comboCount + 1));
         CharacterNode.CharacterSprite.AnimationFinished += HandleAnimationFinished;
+
+        _comboCount = ++_comboCount % _maxComboCount;
+        _timer.Stop();
     }
 
     private void HandleAnimationFinished()
     {
-        _comboCount = ++_comboCount % _maxComboCount;
         CharacterNode.StateMachine.SwitchState<PlayerIdleState>();
     }
 
@@ -23,5 +40,6 @@ public partial class PlayerAttackState : PlayerState
     {
         base.ExitState();
         CharacterNode.CharacterSprite.AnimationFinished -= HandleAnimationFinished;
+        _timer.Start(_comboTime);
     }
 }
