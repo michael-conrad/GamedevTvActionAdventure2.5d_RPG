8d9ed14c1c57af101c0dc42aafb59d29c8d04c34
Michael Conrad <m.conrad.202@gmail.com>
2025-02-03 23:04:22 -0500

    7.4 Starting the Game
    Add game start functionality and refactor resource references
    
    - Introduced a "Start" action in `UiContainer` for starting the game.
    - Added input handling to unpause the game when "Start" is pressed.
    - Updated `Main` scene to initialize in a paused state.
    - Modified UI structure with `StartButton` references and visibility changes.
    - Refactored resource identifiers across player, enemy, and level scenes for consistency.
    - Adjusted project input map by replacing "XBoxMenu" with "Start" binding.

--- Scripts/Levels/Main.cs
+++ Scripts/Levels/Main.cs
@@ -4,4 +4,9 @@ namespace GamedevTvActionAdventure25d_RPG.Scripts.Levels;
 
 public partial class Main : Node3D
 {
+    public override void _Ready()
+    {
+        base._Ready();
+        GetTree().Paused = true;
+    }
 }
--- Scripts/UI/UiContainer.cs
+++ Scripts/UI/UiContainer.cs
@@ -5,4 +5,21 @@ namespace GamedevTvActionAdventure25d_RPG.Scripts.UI;
 public partial class UiContainer : CenterContainer
 {
     [Export] public ContainerType Container { get; private set; }
+    [Export] public Button StartButton { get; private set; }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+        if (!Visible)
+        {
+            return;
+        }
+
+        if (Input.IsActionJustPressed("Start"))
+        {
+            // do start the game
+            Visible = false;
+            GetTree().Paused = false;
+        }
+    }
 }
