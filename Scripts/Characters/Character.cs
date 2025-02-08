6e77408ca2299b2bb5dff72b3073ebf805440320
Michael Conrad <m.conrad.202@gmail.com>
2025-02-08 14:13:52 -0500

    8.2 Preparing the Reward Feature
    Refactor Character Stats and Update Tile Resources
    
    Refactored the `Duplicate` method call in `Character.cs` to include a parameter for more explicit resource duplication. Updated image references and associated IDs in `tiles.tres` to align with new resource identifiers. These changes ensure proper handling and referencing of resources in the project.

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -47,7 +47,8 @@ public abstract partial class Character : CharacterBody3D
         // horrible initial starting point of having shared resources...
         // for (var i = 0; i < Stats.Length; i++)
         // {
-        //     Stats[i] = (StatResource)Stats[i].Duplicate();
+        //     StatResource statResource = Stats[i];
+        //     Stats[i] = (StatResource)statResource.Duplicate(true);
         // }
         // I really find setting these via the Godot editor asinine. So no export for you deary!
         AttackArea = GetNodeOrNull<Area3D>("AttackArea");
