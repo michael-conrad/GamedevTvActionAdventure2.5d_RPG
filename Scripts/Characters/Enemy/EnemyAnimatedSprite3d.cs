64071a0d30f65a492d7fdbe21e34efd0b60969c3
Michael Conrad <m.conrad.202@gmail.com>
2025-06-12 19:08:09 -0400

    Applied script from: https://github.com/Zorochase/ultimate-retro-shader-collection/tree/55934d725ec7c8faf807059a8e8fdc151ff58944
    Sprites do not display animations in editor, but seem to work correctly in running game.

---
+++ Scripts/Characters/Enemy/EnemyAnimatedSprite3d.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+namespace GamedevTvActionAdventure25d_RPG.Scripts.Characters.Player;
+
+public partial class EnemyAnimatedSprite3d : AnimatedSprite3D
+{
+    private ShaderMaterial _shaderMaterial;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _shaderMaterial = (ShaderMaterial)MaterialOverride;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        _shaderMaterial.SetShaderParameter("tex", SpriteFrames.GetFrameTexture(Animation, Frame));
+        // _shaderMaterial.SetShaderParameter("billboard_mode", Billboard);
+    }
+}
